cd7fc2a75732ad40954c0d8e8a9429a913f569f9
Jonathan Azoff <jon@azoffdesign.com>
2013-10-21 21:07:07 -0700

    Added first activity

---
+++ Tahoe/src/main/java/com/lake/tahoe/activities/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.lake.tahoe.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.lake.tahoe.R;
+
+public class FirstActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_first_activity);
+	}
+
+}
