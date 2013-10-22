823d934e59c6cbbbe5902b24e0ee64d9956d6966
Roger Hu <roger.hu@gmail.com>
2013-10-21 21:22:29 -0700

    Add User model.

--- Tahoe/src/main/java/com/lake/tahoe/models/User.java
+++ Tahoe/src/main/java/com/lake/tahoe/models/User.java
@@ -1,8 +1,25 @@
 package com.lake.tahoe.models;
 
+import android.content.Context;
+
+import com.orm.SugarRecord;
+
 /**
  * Created on 10/21/13.
  */
-public class User {
+public class User extends SugarRecord<User> {
+    String name;
+    String email;
+
+    public User(Context ctx) {
+        super(ctx);
+    }
+
+    public User(Context ctx, String name, String email) {
+        super(ctx);
+
+        this.name = name;
+        this email = email;
+    }
 
 }
