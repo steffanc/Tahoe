d72fe550b228e28e6c7f4e815feacced3d2029e9
Jonathan Azoff <jon@azoffdesign.com>
2013-10-21 20:53:03 -0700

    First model

---
+++ Tahoe/src/main/java/com/lake/tahoe/applications/TahoeApp.java
@@ -0,0 +1,10 @@
+package com.lake.tahoe.applications;
+
+import com.orm.SugarApp;
+
+/**
+ * Created on 10/21/13.
+ */
+public class TahoeApp extends SugarApp {
+
+}
---
+++ Tahoe/src/main/java/com/lake/tahoe/models/User.java
@@ -0,0 +1,8 @@
+package com.lake.tahoe.models;
+
+/**
+ * Created on 10/21/13.
+ */
+public class User {
+
+}
