c97f4a6a5e555314ca74a133b67ded3c1ba3ac54
Jonathan Azoff <jon@azoffdesign.com>
2013-10-21 21:37:38 -0700

    Added tabs to User model

--- Tahoe/src/main/java/com/lake/tahoe/models/User.java
+++ Tahoe/src/main/java/com/lake/tahoe/models/User.java
@@ -8,18 +8,19 @@ import com.orm.SugarRecord;
  * Created on 10/21/13.
  */
 public class User extends SugarRecord<User> {
-    String name;
-    String email;
 
-    public User(Context ctx) {
-        super(ctx);
-    }
+	String name;
+	String email;
 
-    public User(Context ctx, String name, String email) {
-        super(ctx);
+	public User(Context ctx) {
+		super(ctx);
+	}
 
-        this.name = name;
-        this.email = email;
-    }
+	public User(Context ctx, String name, String email) {
+		super(ctx);
+
+		this.name = name;
+		this.email = email;
+	}
 
 }
