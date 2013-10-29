9fa2c5c8941c14d5d39c310c9a8c7b3059e6c787
Jonathan Azoff <jon@azoffdesign.com>
2013-10-28 19:14:17 -0700

    - Imports the parse SDK - Adds parse key and secret - Converts Sugar models to ParseObjects

--- Tahoe/src/main/java/com/lake/tahoe/apps/TahoeApp.java
+++ Tahoe/src/main/java/com/lake/tahoe/apps/TahoeApp.java
@@ -1,10 +1,25 @@
 package com.lake.tahoe.apps;
 
-import com.orm.SugarApp;
+import android.app.Application;
+import com.lake.tahoe.R;
+import com.lake.tahoe.models.User;
+import com.parse.Parse;
+import com.parse.ParseUser;
 
 /**
  * Created on 10/21/13.
  */
-public class TahoeApp extends SugarApp {
+public class TahoeApp extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		setupParseSdk();
+	}
+
+	public void setupParseSdk() {
+		ParseUser.registerSubclass(User.class);
+		Parse.initialize(this, getString(R.string.parse_client_id), getString(R.string.parse_key));
+	}
 
 }
