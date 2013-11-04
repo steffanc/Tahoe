70cdebafc859ed0ea65f7178e071f6e25cc754f3
Jonathan Azoff <jon@azoffdesign.com>
2013-11-03 18:30:20 -0800

    PayPal SDK Integration
    - Adds paypal SDK libs
    - Adds PayPal credentials to manifest
    - Adds PayPal environment setting ("sandbox")
    - Adds Camera Permissions for PayPal card scanning
    - Adds Camera features for PayPal card scanning
    - Defines services and activites for PayPal and CardIO
    - Adds PendingPaymentActivity and tests PayPal integration
    - Updates the Request class to expose its bid value in PayPal-friendly getters
    - Updates Helpers to add a vendor in the mock Request

--- src/com/lake/tahoe/utils/Helpers.java
+++ src/com/lake/tahoe/utils/Helpers.java
@@ -7,25 +7,33 @@ import com.lake.tahoe.models.User;
  * Created by steffan on 11/3/13.
  */
 public class Helpers {
+
 	public static Request createMockRequest() {
 		// Sample data for now
 		// TODO remove this sample data in the future
 		// TODO move this initialization to the constructor
-		User client1 = new User();
-		client1.setName("Herp Derpington");
-		client1.setLocation(37.7583, -122.4275);
-
-		Request request1 = new Request();
-		request1.setTitle("Beer Wanted");
-		request1.setDescription(
+		Request request = new Request(Request.State.OPEN);
+		request.setTitle("Beer Wanted");
+		request.setDescription(
 				"It's a beautiful day, I've got a sandwich, " +
 				"but I lack a beverage. When considering what beverage " +
 				"I'd enjoy, I decided on a beer. Please, I need a beer."
 		);
-		request1.setClient(client1);
-		request1.setCents(150);
+		request.setCents(150);
 
-		return request1;
+		User client = new User();
+		client.setName("Herp Derpington");
+		client.setLocation(37.7583, -122.4275);
+		client.setEmail("jazoff-facilitator@gmail.com");
+		request.setClient(client);
+
+		User vendor = new User();
+		vendor.setName("Derp Herpington");
+		vendor.setLocation(37.7583, -122.4275);
+		vendor.setEmail("jazoff+vendor@gmail.com");
+		request.setVendor(vendor);
+
+		return request;
 	}
 
 }
